b4add4c573393a415bf3e5599092a1f666a15643
Bert Carels <bertcarels@gmail.com>
2026-06-29 22:32:41 +0200

    cloudflae insights

---
+++ inject-analytics.mjs
@@ -0,0 +1,12 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+const BEACON = `<script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "4fc7f960d6ed4dd18f3c206380c11b5b"}'></script>`;
+const file = 'dist/client/index.html';
+
+const html = readFileSync(file, 'utf8');
+if (html.includes('cloudflareinsights')) {
+  console.log('Analytics beacon already present in', file);
+} else {
+  writeFileSync(file, html.replace('</head>', `  ${BEACON}\n</head>`));
+  console.log('Analytics beacon injected into', file);
+}
